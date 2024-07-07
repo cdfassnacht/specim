ea5e858e7e1926f4dfab062ec776a4e3ba479749
Chris Fassnacht <cdfassnacht@ucdavis.edu>
2024-07-06 18:36:29 -0700

    Temporarily(?) remove python 3 req from specim

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    # python_requires='>=3.6',
     install_requires=[
         'numpy>=1.10',
         'scipy>=1.1',
